e7a11b6659d2ba9de15704dfa6e23a59a8f378fc
Chuk <65512186+Malinariy@users.noreply.github.com>
2021-10-31 18:29:19 +0200

    Add files via upload

---
+++ rpg_game/game_object.sol
@@ -0,0 +1,41 @@
+pragma ton-solidity >= 0.35.0;
+pragma AbiHeader expire;
+
+import 'igo.sol';
+
+contract game_object is game_interface {
+
+    uint health = 50;
+    uint  def_value = 0;
+
+    function get_attack(uint damage) external override {
+        health = health + (def_value - damage);
+        if (check_death() == true){
+            death_ceremony(msg.sender);
+        }
+    }
+
+    function check_death() private view returns (bool){
+        return health <= 0;
+    }
+
+    function get_def_value(uint value)virtual public checkOwnerAndAccept{
+        tvm.accept();
+        def_value += value;
+    }
+
+    function death_ceremony(address murderer)virtual internal {
+        self_destruction(murderer);
+    }
+
+    function self_destruction(address murderer)virtual internal {
+        murderer.transfer(0, true, 160);
+    }
+
+    modifier checkOwnerAndAccept() {
+        require(msg.pubkey() == tvm.pubkey(), 102, 'You are not the owner!');
+        tvm.accept();
+        _;
+    }
+
+}
---
+++ rpg_game/military_unit.sol
@@ -0,0 +1,37 @@
+pragma ton-solidity >= 0.35.0;
+pragma AbiHeader expire;
+
+import 'game_object.sol';
+import 'base_station.sol'; 
+
+contract military_unit is game_object {
+
+    uint  atk_value;
+    address public address_base;
+
+    constructor(base_station base) public {
+        require(msg.pubkey() == tvm.pubkey(), 101, 'Constructor error');
+		tvm.accept();
+        base.create_unit();
+        address_base = address(base);
+    }
+
+    function get_attack_value(uint value) virtual public checkOwnerAndAccept{
+        atk_value = value;
+    }
+
+    function attack(game_interface gameObjectInterface) public checkOwnerAndAccept{
+        gameObjectInterface.get_attack(atk_value);
+    }
+
+    function death_ceremony(address murderer) internal override{
+        base_station(address_base).delete_unit();
+        self_destruction(murderer);
+    }
+
+    function death_by_base_station(address murderer) public {
+        tvm.accept();
+        require(msg.sender == address_base, 102, "You are not the owner!");
+        death_ceremony(murderer);
+    }
+}
