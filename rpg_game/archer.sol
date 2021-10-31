34101204b9d75243ede790dd29bede199ad18d99
Chuk <65512186+Malinariy@users.noreply.github.com>
2021-10-31 18:27:25 +0200

    archer files

---
+++ rpg_game/archer.sol
@@ -0,0 +1,19 @@
+pragma ton-solidity >= 0.35.0;
+pragma AbiHeader expire;
+
+import 'military_unit.sol';
+
+contract archer is military_unit {
+
+   constructor(base_station base, uint atk, uint def) military_unit(base) public {
+      get_attack_value(atk);
+      get_def_value(def);
+   }
+
+   function get_attack_value(uint value) public override checkOwnerAndAccept{
+      atk_value += value;
+   }
+   function get_def_value(uint value) public override checkOwnerAndAccept{
+      def_value += value;
+   }
+}
